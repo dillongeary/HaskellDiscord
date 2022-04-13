1414793aaded9f8da540e086d2c70b5b2939399b
Dillon <dgfg1g20@soton.ac.uk>
2022-04-13 15:58:06 +0100

    cleaned up Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,26 +18,16 @@ import qualified Control.Monad as Prelude
 
 data Outcomes a = Rolls [a] | Modifier a deriving (Ord,Eq, Show)
 
-outcomesSum :: [Outcomes Int] -> Int
-outcomesSum [] = 0
-outcomesSum (Rolls x:xs) = sum x + outcomesSum xs
-outcomesSum (Modifier x:xs) = x + outcomesSum xs
-
-list2tuple :: [a] -> (a,a)
-list2tuple [a,b] = (a,b)
-
--- | Replies "pong" to every message that starts with "ping"
 discordMain :: String -> IO ()
-discordMain token = do
-    userFacingError <- runDiscord $ def
-             { discordToken = pack token
-             , discordOnEvent = eventHandler
-             , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
-             } -- if you see OnLog error, post in the discord / open an issue
+discordMain token = do userFacingError <- runDiscord $ def
+                         { discordToken = pack token
+                         , discordOnEvent = eventHandler
+                         , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
+                         } -- if you see OnLog error, post in the discord / open an issue
 
-    TIO.putStrLn userFacingError
-    -- userFacingError is an unrecoverable error
-    -- put normal 'cleanup' code in discordOnEnd (see examples)
+                       TIO.putStrLn userFacingError
+                       -- userFacingError is an unrecoverable error
+                       -- put normal 'cleanup' code in discordOnEnd (see examples)
 
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
@@ -45,6 +35,14 @@ eventHandler event = case event of
                                                               output <- rollDice m $ replace "-" "+-" (messageContent m)
                                                               void $ restCall $ R.CreateMessage (messageChannelId m) output
     _ -> return ()
+  where fromBot :: Message -> Bool
+        fromBot = userIsBot . messageAuthor
+
+        isDie :: Message -> Bool
+        isDie x = replace " " "" (messageContent x) =~ diceRegex :: Bool
+          where diceRegex :: String
+                diceRegex = "^(([1-9][0-9]?)?[d|D][1-9][0-9]*|[-]?[0-9]+)([+]([1-9][0-9]?)?[d|D][1-9][0-9]*|[+|-]?[0-9]+)*$"
+
 
 rollDice :: (Control.Monad.IO.Class.MonadIO m) => Message -> Text -> m Text
 rollDice m xs = do gen <- newStdGen
@@ -58,10 +56,12 @@ rollDice m xs = do gen <- newStdGen
         rolls = Prelude.filter (\x -> Prelude.length x == 2) dices
         dices :: [[Int]]
         dices = Prelude.map (Prelude.map (smartRead . unpack) . Data.Text.split (\x -> x == 'D' || x == 'd')) (Data.Text.split (== '+') xs)
-
-foo :: (Random (m a), Monad m, Num a, MonadIO ((->) (Integer, a))) => (Int,a) -> m (Outcomes a)
-foo (a,s) = fmap Rolls (replicateM a (randomIO (1,s)))
-
+        list2tuple :: [a] -> (a,a)
+        list2tuple [a,b] = (a,b)
+        list2tuple _ = error "Invalid Tuple List"
+        smartRead :: String -> Int
+        smartRead [] = 1
+        smartRead xs = read xs
 
 getRolls :: (Random a, RandomGen t, Num a) => t -> [(Int,a)] -> [Outcomes a]
 getRolls g [] = []
@@ -70,12 +70,6 @@ getRolls g ((a,s):xs) = Rolls o : getRolls ng xs
         doRolls [] g os = (os,g)
         doRolls (x:xs) g os = doRolls xs bar (foo:os)
           where (foo,bar) = randomR (1,x) g
-
-smartRead :: String -> Int
-smartRead [] = 1
-smartRead xs = read xs
-
---TODO = Replace You with user Mention
 
 prettyPrint :: Message -> [Outcomes Int] -> String
 prettyPrint m os = "<@" ++ username ++ "> rolled " ++ stuff os
@@ -93,26 +87,7 @@ prettyPrint m os = "<@" ++ username ++ "> rolled " ++ stuff os
                         extraBreakdown t [] = error "aaaaaaa"
                         extraBreakdown t [x] = show x ++ " = " ++ show (t+x)
                         extraBreakdown t (x:xs) = show x ++ " + " ++ extraBreakdown (t+x) xs
-
-
-{- prettyPrint :: Message -> Int -> ([Int],[Int]) -> String
-prettyPrint m total ([r],[]) = "<@" ++ username ++ "> rolled **" ++ show total ++ "**"
-  where username = show $ fromEnum $ userId $ messageAuthor m
-prettyPrint m total (rolls,modifiers) = "<@" ++ username ++ "> rolled **" ++ show total ++ "**   (" ++ formatRolls rolls 0 ++ formatModifiers modifiers ++ ")"
-  where formatRolls :: [Int] -> Int -> String
-        formatRolls [] _ = ""
-        formatRolls [x] 0 = show x
-        formatRolls [x] i = show x ++ " = " ++ show (i + x)
-        formatRolls (x:xs) i = show x ++ " + " ++ formatRolls xs (i+x)
-        formatModifiers :: [Int] -> String
-        formatModifiers [] = ""
-        formatModifiers xs = Prelude.concatMap (\x -> ", " ++ show x) xs
-        username = show $ fromEnum $ userId $ messageAuthor m -}
-
-fromBot :: Message -> Bool
-fromBot = userIsBot . messageAuthor
-
-isDie :: Message -> Bool
-isDie x = replace " " "" (messageContent x) =~ diceRegex :: Bool
-  where diceRegex :: String
-        diceRegex = "^(([1-9][0-9]?)?[d|D][1-9][0-9]*|[-]?[0-9]+)([+]([1-9][0-9]?)?[d|D][1-9][0-9]*|[+|-]?[0-9]+)*$"
+                outcomesSum :: [Outcomes Int] -> Int
+                outcomesSum [] = 0
+                outcomesSum (Rolls x:xs) = sum x + outcomesSum xs
+                outcomesSum (Modifier x:xs) = x + outcomesSum xs
